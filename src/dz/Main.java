43922542be11cf40189d1035b9f307d6a0390f2c
asrytikov <asrytikov@gmail.com>
2023-10-25 10:29:57 +0400

    Initial commit

---
+++ src/dz/Main.java
@@ -0,0 +1,32 @@
+import dz.Task;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Task> taskList = new ArrayList<>();
+        Task task = new Task(1, "New task");
+        System.out.println(task);
+        taskList.add(task);
+        saveListFile(taskList);
+    }
+
+    private static void saveListFile(List<Task> taskList){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\test\\tasks.txt", false))){
+            String t = "";
+            for (int i = 0; i < taskList.size(); i++){
+                t += taskList.get(i);
+            }
+            writer.write(t);
+            System.out.println("DONE");
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+}
